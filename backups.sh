ac5164f877276ebd44359aff96ec7b26209f738c
Ubuntu <ubuntu@ip-172-31-37-8.eu-west-1.compute.internal>
2025-06-06 09:08:24 +0000

    testu

---
+++ backups.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source_dir="/home/ubuntu/shellscription"
+dest_dir="/home/ubuntu/backup"
+function create_backup(){
+
+timestamp=$(date  '+%Y-%m-%d-%H-%M-%S')
+
+backup_dir="$dest_dir/backup_${timestamp}"
+
+zip -r "${backup_dir}.zip" "$source_dir"
+}
+
+create_backup $source_dir $dest_dir
